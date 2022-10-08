77ce32ff1fd183c61a996a28bc9bccc9eb69b8af
MurugeshK <kmurugeshr14@gmail.com>
2022-10-08 17:46:26 +0530

    Set Up: Project Structure, Theme, Navigation & Hilt

---
+++ app/src/main/java/com/kaliondroid/techarena/HomeFragment.kt
@@ -0,0 +1,29 @@
+package com.kaliondroid.techarena
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.kaliondroid.techarena.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.textBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_searchFragment)
+        }
+    }
+}
--- app/src/main/java/com/kaliondroid/techarena/MainActivity.kt
+++ app/src/main/java/com/kaliondroid/techarena/MainActivity.kt
@@ -2,10 +2,14 @@ package com.kaliondroid.techarena
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kaliondroid.techarena.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
